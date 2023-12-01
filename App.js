ed7262ed8e552710ff7fac8a11976548f0b47c32
Your Name <dinitayala@gmail.com>
2023-11-30 21:38:25 -0500

    Add: Plantilla Alumno

---
+++ App.js
@@ -0,0 +1,15 @@
+import 'react-native-gesture-handler';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import HomeScreen from "./screens/Home";
+import IssLocationScreen from "./screens/IssLocation";
+import MeteorScreen from "./screens/Meteors";
+import UpdateScreen from "./screens/Updates";
+
+export default function App() {
+  return (
+    <Text>Inicio de Código</Text>
+  );
+}
